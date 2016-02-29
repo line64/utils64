25d71e90aa11d6bfc14d864b15153e3158c97599
Nicolas Echezarreta <nicolas.echezarreta@line64.com>
2016-02-29 15:12:15 -0300

    [FEATURE] Add Broccoli as build tool

---
+++ Brocfile.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const concat = require('broccoli-concat');
+const babel = require('broccoli-babel-transpiler');
+const pkg = require('./package.json');
+
+const src = 'src';
+
+const js = babel(src, {
+  stage: 0,
+  moduleIds: true,
+  modules: 'amd',
+
+  getModuleId: function (name) {
+    name = pkg.name + '/' + name;
+    return name.replace(/\/index$/, '');
+  },
+
+  resolveModuleSource: function (source, filename) {
+    var match = filename.match(/(.+)\/index\.\S+$/i);
+
+    if (match) {
+      var path = match[1];
+      return source
+        .replace(/^\.\//, path + '/')
+        .replace(/^\.\.\//, '');
+    } else {
+      return source;
+    }
+  }
+
+});
+
+const main = concat(js, {
+  inputFiles: [
+    '**/*.js'
+  ],
+  outputFile: 'index.js'
+});
+
+module.exports = main;
