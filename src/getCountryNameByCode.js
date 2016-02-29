84ce30dcfce0c3e633da5eaac802fc492177a269
Nicolas Echezarreta <nicolas.echezarreta@line64.com>
2016-02-29 15:55:27 -0300

    [UTIL] Add getCountryNameByCode util

---
+++ src/getCountryNameByCode.js
@@ -0,0 +1,14 @@
+"use strict";
+
+import Countries from "../json/countries.json";
+import { find } from "lodash";
+
+export default function getCountryName(code) {
+
+    let found = find(Countries.countries, (country) => country.code === code.toUpperCase());
+
+    if (found) return found.country;
+
+    return code;
+
+}
